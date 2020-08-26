9995ec78d91bf351bcf7b40258255d44404e0afa
SebEil <56046884+SebEil@users.noreply.github.com>
2020-08-26 13:08:27 +0200

    Update

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -3,10 +3,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YatzyTest {
+
+    private YatzyGame yatzyGame = new YatzyGame();
+
     @Test
     void shouldScoreOnes(){
-        assertEquals(5, new YatzyGame().score(YatzyCategory.ONES, new int[] {1,1,1,1,1}));
-
+        assertEquals(5, yatzyGame.score(YatzyCategory.ONES, new int[] {1,1,2,1,1}));
+        assertEquals(5, yatzyGame.score(YatzyCategory.ONES, new int[] {1,1,2,1,1}));
     }
 
 }
